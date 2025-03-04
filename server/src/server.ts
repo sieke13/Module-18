7bf2408154ce2eda9a98067b2a8f391b6e484284
sieke13 <gera.ave2@gmail.com>
2025-03-03 19:06:05 -0600

    3.5

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,8 +7,8 @@ import dotenv from 'dotenv';
 
 // Importaciones de archivos locales (con extensiones .js)
 import connectDB from './config/connection.js';
-import {typeDefs} from './schemas/typeDefs.js';
-import resolvers from './schemas/resolvers.js';
+import {typeDefs, resolvers} from './schemas/index.js';
+
 import { authMiddleware } from './services/auth.js';
 
 // Configuración básica
